17c7cfe227791b38815a0dab4f21975dfe41c981
arkodiy <60521446+arkodiy@users.noreply.github.com>
2020-02-15 23:18:14 +0500

    disable homing_backoff

--- Configuration_adv.h
+++ Configuration_adv.h
@@ -524,7 +524,7 @@
 #define Z_HOME_BUMP_MM 5
 #define HOMING_BUMP_DIVISOR { 4, 4, 4 }  // Re-Bump Speed Divisor (Divides the Homing Feedrate)
 #define QUICK_HOME                     // If homing includes X and Y, do a diagonal move initially
-#define HOMING_BACKOFF_MM { 0, 0, 15 }  // (mm) Move away from the endstops after homing
+#define HOMING_BACKOFF_MM { 0, 0, 0 }  // (mm) Move away from the endstops after homing
 
 // When G28 is called, this option will make Y home before X
 //#define HOME_Y_BEFORE_X
